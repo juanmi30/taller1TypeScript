912b7885cf79db4a6e30b1c32c665526f3b4f383
juanmi30 <j.manriquec@uniandes.edu.co>
2024-04-13 21:57:26 -0500

    commit final taller 2

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { series } from "./data.js";
 import { Serie } from "./serie.js";
 
 let seriesTbody: HTMLElement = document.getElementById("series")!;
+let card: HTMLElement = document.getElementById("card")!;
+let cardBody: HTMLElement = document.getElementById("card-body")!;
 
 renderSeriesInTable(series);
 
@@ -10,7 +12,7 @@ function renderSeriesInTable(series: Serie[]): void {
   series.forEach((serie) => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${serie.id}</td> 
-                            <td><a href="#">${serie.nombre}</a></td>
+                            <td><a href="#" id="${serie.id}" class="serie-link">${serie.nombre}</a></td>
                             <td>${serie.canal}</td>
                             <td>${serie.temporadas}</td>`;
     seriesTbody.appendChild(trElement);
@@ -20,6 +22,17 @@ function renderSeriesInTable(series: Serie[]): void {
     series
   )}`;
   seriesTbody.appendChild(trPromedio);
+
+  const serieLinks = document.querySelectorAll(".serie-link");
+  serieLinks.forEach((link) => {
+    link.addEventListener("click", (Event) => {
+      Event.preventDefault();
+      const serieId: string = link.getAttribute("id")!;
+      let serieActual: Serie = series[+serieId - 1];
+      console.log(serieActual);
+      renderCard(serieActual);
+    });
+  });
 }
 
 function getPromedioTemporadas(series: Serie[]): number {
@@ -28,4 +41,30 @@ function getPromedioTemporadas(series: Serie[]): number {
     numTemporadasTotal = numTemporadasTotal + serie.temporadas;
   });
   return numTemporadasTotal / series.length;
+}
+
+function renderCard(serie: Serie) {
+  console.log("Desplegando card");
+  card.style.display = "flex";
+  card.innerHTML = `<img class="card-img-top" src="${serie.urlImagen}" alt="${serie.nombre}">`;
+
+  let h1Element = document.createElement("h1");
+  h1Element.classList.add("card-title");
+  h1Element.innerText = serie.nombre;
+
+  let descripcion = document.createElement("p");
+  descripcion.classList.add("card-text");
+  descripcion.innerText = serie.descripcion;
+
+  let linkInfo = document.createElement("a");
+  linkInfo.classList.add("card-text");
+  linkInfo.innerText = serie.webOficial;
+  linkInfo.setAttribute("href", serie.webOficial);
+  linkInfo.setAttribute("target", "_blank");
+
+  cardBody.innerHTML = "";
+  cardBody.appendChild(h1Element);
+  cardBody.appendChild(descripcion);
+  cardBody.appendChild(linkInfo);
+  card.appendChild(cardBody);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,28 @@
 import { series } from "./data.js";
 var seriesTbody = document.getElementById("series");
+var card = document.getElementById("card");
+var cardBody = document.getElementById("card-body");
 renderSeriesInTable(series);
 function renderSeriesInTable(series) {
     console.log("Desplegando series");
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td> \n                            <td><a href=\"#\">").concat(serie.nombre, "</a></td>\n                            <td>").concat(serie.canal, "</td>\n                            <td>").concat(serie.temporadas, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td> \n                            <td><a href=\"#\" id=\"").concat(serie.id, "\" class=\"serie-link\">").concat(serie.nombre, "</a></td>\n                            <td>").concat(serie.canal, "</td>\n                            <td>").concat(serie.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
     });
     var trPromedio = document.createElement("p");
     trPromedio.innerHTML = "Promedio de temporadas: ".concat(getPromedioTemporadas(series));
     seriesTbody.appendChild(trPromedio);
+    var serieLinks = document.querySelectorAll(".serie-link");
+    serieLinks.forEach(function (link) {
+        link.addEventListener("click", function (Event) {
+            Event.preventDefault();
+            var serieId = link.getAttribute("id");
+            var serieActual = series[+serieId - 1];
+            console.log(serieActual);
+            renderCard(serieActual);
+        });
+    });
 }
 function getPromedioTemporadas(series) {
     var numTemporadasTotal = 0;
@@ -18,4 +30,25 @@ function getPromedioTemporadas(series) {
         numTemporadasTotal = numTemporadasTotal + serie.temporadas;
     });
     return numTemporadasTotal / series.length;
+}
+function renderCard(serie) {
+    console.log("Desplegando card");
+    card.style.display = "flex";
+    card.innerHTML = "<img class=\"card-img-top\" src=\"".concat(serie.urlImagen, "\" alt=\"").concat(serie.nombre, "\">");
+    var h1Element = document.createElement("h1");
+    h1Element.classList.add("card-title");
+    h1Element.innerText = serie.nombre;
+    var descripcion = document.createElement("p");
+    descripcion.classList.add("card-text");
+    descripcion.innerText = serie.descripcion;
+    var linkInfo = document.createElement("a");
+    linkInfo.classList.add("card-text");
+    linkInfo.innerText = serie.webOficial;
+    linkInfo.setAttribute("href", serie.webOficial);
+    linkInfo.setAttribute("target", "_blank");
+    cardBody.innerHTML = "";
+    cardBody.appendChild(h1Element);
+    cardBody.appendChild(descripcion);
+    cardBody.appendChild(linkInfo);
+    card.appendChild(cardBody);
 }
